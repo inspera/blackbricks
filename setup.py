5d701e3055741034791d3fe658847052f42310c4
Bendik Samseth <b.samseth@gmail.com>
2020-07-30 12:59:53 +0200

    Update install dependencies to include databricks-cli

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=[
         "black==19.10b0",
         "sqlparse==0.3.1",
+        "databricks-cli==0.11.0",
         "typer==0.3.0",
         "colorama==0.4.3",
     ],
